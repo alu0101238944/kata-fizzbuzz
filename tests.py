e7ce18bb1961b2d3af2984a5b0e031aa4ba73aee
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-03-30 00:24:51 +0100

    Red: Give 5 return Buzz

--- tests.py
+++ tests.py
@@ -1,6 +1,5 @@
 
 import unittest
-
 from fizzbuzz import FizzBuzz
 
 class FizzBuzzTests(unittest.TestCase):
@@ -16,6 +15,9 @@ class FizzBuzzTests(unittest.TestCase):
   def test_give_multiple_of_3_return_fizz(self):
     for i in range(100):
       self.assertEqual(self.fizz_buzz.int_to_string(3 * i), 'Fizz')
+
+  def test_give_5_return_buzz(self):
+    self.assertEqual(self.fizz_buzz.int_to_string(5), 'Buzz')
 
 if __name__ == '__main__':
   unittest.main()
