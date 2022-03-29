07cba94daeaeb8921171ea7afceef615783c9fc4
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-03-30 00:40:21 +0100

    Refactor: Give 5 return Buzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,4 +3,7 @@ class FizzBuzz:
   def int_to_string(self, value: int):
     if value == 5:
       return 'Buzz'
-    return 'Fizz' if value % 3 == 0 else str(value)
+    elif value % 3 == 0:
+      return 'Fizz'
+    else:
+      return str(value)
