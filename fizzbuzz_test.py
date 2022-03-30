355e21a2a1e6e54be7b807ba85a8858ad2ddb3a0
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-03-30 15:13:25 +0100

    Red: Return fizz if has 3 in it

---
+++ fizzbuzz_test.py
@@ -0,0 +1,33 @@
+
+import unittest
+from src.fizzbuzz import FizzBuzz
+
+class FizzBuzzTests(unittest.TestCase):
+  def __init__(self, *args):
+    super().__init__(*args)
+    self.fizzbuzz = FizzBuzz()
+
+  def test_returns_number_string(self):
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(1), '1')
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(4), '4')
+
+  def test_returns_fizz_if_multiple_of_3(self):
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(3), 'Fizz')
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(18), 'Fizz')
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(363), 'Fizz')
+
+  def test_returns_buzz_if_multiple_of_5(self):
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(5), 'Buzz')
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(20), 'Buzz')
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(425), 'Buzz')
+   
+  def test_returns_fizzbuzz_if_multiple_of_3_and_5(self):
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(15), 'FizzBuzz')
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(720), 'FizzBuzz')
+
+  def test_returns_fizz_if_has_3_in_it(self):
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(13), 'Fizz')
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(32), 'Fizz')
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(12368), 'Fizz')
+
+unittest.main()
---
+++ src/fizzbuzz.py
@@ -0,0 +1,27 @@
+
+class FizzBuzz():
+  def to_fizzbuzz(self, number: int):
+    result = ''
+    if self.is_fizz(number):
+      result += 'Fizz'
+    if self.is_buzz(number):
+      result += 'Buzz'
+    return str(number) if result == '' else result
+    
+  def is_fizz(self, number: int):
+    return self.get_recursive_digit_sum(number) in [3, 6, 9]
+    
+  def get_recursive_digit_sum(self, number):
+    return (number if number < 10
+      else self.get_recursive_digit_sum(self.get_digit_sum(number))
+    )
+
+  def get_digit_sum(self, number: int):
+    digit_sum = 0
+    while number > 0:
+      digit_sum += number % 10
+      number //= 10
+    return digit_sum
+
+  def is_buzz(self, number: int):
+    return number % 10 in [0, 5]
