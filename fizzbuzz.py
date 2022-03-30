6a45184f495edaa4662c8426cb52a36df04a92a1
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-03-30 12:24:12 +0100

    Green: Give 5 return Buzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,4 +6,6 @@ class FizzBuzz:
   def int_to_string(self, value: int):
     if value % 3 == 0 or '3' in str(value):
       return self.fizz
+    if value == 5:
+      return 'Buzz'
     return str(value)
