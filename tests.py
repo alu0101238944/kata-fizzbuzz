7efc33d08c5f5ae3ffa403fad6d565ff958613db
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-03-30 12:10:38 +0100

    Red: Give 3 return Fizz

--- tests.py
+++ tests.py
@@ -9,5 +9,8 @@ class FizzBuzzTests(unittest.TestCase):
   def test_give_1_return_1(self):
     self.assertEqual(self.fizz_buzz.int_to_string(1), '1')
 
+  def test_give_3_return_fizz(self):
+    self.assertEqual(self.fizz_buzz.int_to_string(3), 'Fizz')
+
 if __name__ == '__main__':
   unittest.main()
