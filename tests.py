f79a1057b0251f3e2121fe08f959c373dd9f0de2
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-03-30 12:30:27 +0100

    Red: Give multiple of 5 or has 5 in it return Buzz

--- tests.py
+++ tests.py
@@ -19,7 +19,7 @@ class FizzBuzzTests(unittest.TestCase):
       multiples_only_of_3 += list(range(1 + i, 100, 5))
     for i in multiples_only_of_3:
       # If has a 3 return Fizz
-      if '3' in str(i):
+      if '3' in str(i) and '5' not in str(i):
         self.assertEqual(self.fizz_buzz.int_to_string(i), 'Fizz')
       # Else multiple of 3 return Fizz
       else:
@@ -27,6 +27,19 @@ class FizzBuzzTests(unittest.TestCase):
 
   def test_give_5_return_buzz(self):
     self.assertEqual(self.fizz_buzz.int_to_string(5), 'Buzz')
+
+    # Multiple only of 5 means that is not multiple of 3
+  def test_give_multiple_only_of_5_or_has_5_in_it_return_fizz(self):
+    multiples_only_of_5 = []
+    for i in range(3 - 1):
+      multiples_only_of_5 += list(range(1 + i, 100, 3))
+    for i in multiples_only_of_5:
+      # If has a 5 return Fizz
+      if '5' in str(i) and '3' not in str(i):
+        self.assertEqual(self.fizz_buzz.int_to_string(i), 'Buzz')
+      # Else multiple of 5 return Fizz
+      else:
+        self.assertEqual(self.fizz_buzz.int_to_string(i * 5), 'Buzz')
 
 if __name__ == '__main__':
   unittest.main()
