428b5b5f1ed28fc7f31233b7e355a279edbedaac
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-03-30 12:13:50 +0100

    Refactor: Give 3 return Fizz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,9 @@
 
 class FizzBuzz:
+  def __init__(self):
+    self.fizz = 'Fizz'
+
   def int_to_string(self, value: int):
     if value == 3:
-      return 'Fizz'
+      return self.fizz
     return str(value)
