5f3e4bbf3bda9c50345077b8d45ebcf21ec7493f
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-06 14:17:30 +0100

    Green: Should get state of regular number

--- src/main/java/kata/example/FizzBuzz.java
+++ src/main/java/kata/example/FizzBuzz.java
@@ -1,5 +1,11 @@
 package kata.example;
 
 public class FizzBuzz {
-
-}
+  private int state_ = -1; 
+  public FizzBuzz(int state) {
+    this.state_ = state;
+  }
+  public String getState() {
+    return Integer.toString(this.state_);
+  }
+};
--- src/test/java/kata/example/FizzBuzzTest.java
+++ src/test/java/kata/example/FizzBuzzTest.java
@@ -4,5 +4,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
-
+  @Test
+  void shouldGetStateOfRegularNumber() {
+    FizzBuzz fizzbuzz = new FizzBuzz(1);
+    assertEquals("1", fizzbuzz.getState());
+  }
 }
